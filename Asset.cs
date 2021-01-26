888caa6779448b67db5440707c20bcf9bc1ef185
Srilatha <55086334+srilatha-yanamadala@users.noreply.github.com>
2021-01-26 09:11:45 +0100

    Add files via upload
    Asset tracker mini project

---
+++ Asset.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+
+namespace AssetTracking.Domain
+{
+  
+    public class Asset
+    {
+       
+        public Asset()
+        {
+
+        }
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public DateTime PurchaseDate { get; set; }
+        public Office Office { get; set; }
+        public double PurchasePrice { get; set; }
+        public string Currency { get; set; }
+        public string AssetType { get; set; }
+        public string OfficeLocation { get; }
+    }
+}
